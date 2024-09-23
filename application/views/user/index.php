19462c1f808b7c568d04b5c5a8f6dcf148b50864
unknown <van90861@gmail.com>
2024-09-23 10:20:11 +0700

    first commit

---
+++ application/views/user/index.php
@@ -0,0 +1,7 @@
+<br><br><br>
+    <div class="container text-center" style="margin: 2em auto;">
+      <div class="jumbotron">
+        <h1 class="display-3">SELAMAT DATANG</h1>
+        <p class="lead">Di Inventory Gudang Astra Toll Tamer</p>
+    </div>
+  </div>
